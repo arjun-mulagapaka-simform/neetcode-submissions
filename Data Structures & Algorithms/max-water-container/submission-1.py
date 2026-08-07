35cb32c7ec84fbeec82ca5faa26e6ed37115f4d6
arjun-mulagapaka-simform <arjun.mulagapaka@simformsolutions.com>
2026-08-07 10:56:30 +0530

    Add: max-water-container - submission-1

---
+++ Data Structures & Algorithms/max-water-container/submission-1.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxArea(self, heights: List[int]) -> int:
+        max_water = 0
+        curr_water = 0
+        l,r = 0, len(heights)-1
+        while l < r:
+            curr_water = (r-l) * min(heights[l],heights[r])
+            max_water = max(max_water,curr_water)
+            if heights[l] < heights[r]:
+                l += 1
+            else:
+                r -= 1
+        return max_water
