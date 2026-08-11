28141374d6fe61f3e156e43da985e4c68982164f
arjun-mulagapaka-simform <arjun.mulagapaka@simformsolutions.com>
2026-08-11 17:03:26 +0530

    Add: trapping-rain-water - submission-1

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-1.py
@@ -0,0 +1,13 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        suffix_max = [0 for _ in range(len(height))]
+        suffix_max[-1] = height[-1]
+        for i in range(len(height)-2,-1,-1):
+            suffix_max[i] = max(height[i],suffix_max[i+1])
+        
+        prefix_max = total = 0
+        for i in range(len(height)):
+            prefix_max = max(prefix_max,height[i])
+            total += min(prefix_max,suffix_max[i]) - height[i]
+        
+        return total
